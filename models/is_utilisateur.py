0586597ac5aa5fd5790c80f2463e7ab52f9b9139
Tony Galmiche <tony.galmiche@infosaone.com>
2024-01-15 18:27:39 +0100

    modifs

--- models/is_utilisateur.py
+++ models/is_utilisateur.py
@@ -67,26 +67,26 @@ class is_utilisateur(models.Model):
             if fax != '':
                 fax = u'Fax : '+fax
 
-            # fonction  = self.get_tr(obj.fonction)
-            # telephone = self.get_tr(telephone)
-            # portable  = self.get_tr(portable)
-            # fax       = self.get_tr(fax)
-            # autre     = self.get_tr(obj.autre)
+            fonction  = self.get_tr(obj.fonction)
+            telephone = self.get_tr(telephone)
+            portable  = self.get_tr(portable)
+            fax       = self.get_tr(fax)
+            autre     = self.get_tr(obj.autre)
 
             html = html.replace('${name}'     , (obj.name or ''))
             html = html.replace('${mail}'    , (obj.mail or ''))
 
-            html = html.replace('${fonction}' , (obj.fonction or ''))
-            html = html.replace('${telephone}', (obj.telephone or ''))
-            html = html.replace('${portable}' , (obj.portable or ''))
-            html = html.replace('${fax}'      , (obj.fax or ''))
-            html = html.replace('${autre}'    , (obj.autre or ''))
+            # html = html.replace('${fonction}' , (obj.fonction or ''))
+            # html = html.replace('${telephone}', (obj.telephone or ''))
+            # html = html.replace('${portable}' , (obj.portable or ''))
+            # html = html.replace('${fax}'      , (obj.fax or ''))
+            # html = html.replace('${autre}'    , (obj.autre or ''))
 
-            # html = html.replace('<tr><td>${fonction}</td></tr>' , fonction)
-            # html = html.replace('<tr><td>${telephone}</td></tr>', telephone)
-            # html = html.replace('<tr><td>${portable}</td></tr>' , portable)
-            # html = html.replace('<tr><td>${fax}</td></tr>'      , fax)
-            # html = html.replace('<tr><td>${autre}</td></tr>'    , autre)
+            html = html.replace('<tr><td>${fonction}</td></tr>' , fonction)
+            html = html.replace('<tr><td>${telephone}</td></tr>', telephone)
+            html = html.replace('<tr><td>${portable}</td></tr>' , portable)
+            html = html.replace('<tr><td>${fax}</td></tr>'      , fax)
+            html = html.replace('<tr><td>${autre}</td></tr>'    , autre)
             if html:
                 obj.signature_mail = html
             obj.generer_piece_jointe()
