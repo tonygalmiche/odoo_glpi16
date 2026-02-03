663a3a8f92fb5bd9d4d7bdfc0d698e9df6bcda38
Tony Galmiche <tony.galmiche@infosaone.com>
2026-02-03 08:51:04 +0100

    modifs

--- models/is_ordinateur.py
+++ models/is_ordinateur.py
@@ -61,19 +61,20 @@ class is_ordinateur(models.Model):
     suivi_sauvegarde_nb  = fields.Integer('Nb suivi sauvegarde', compute='_compute', readonly=True, store=False)
     active               = fields.Boolean('Actif', default=True)
 
-    glpi_name              = fields.Char('Nom du poste GLPI' , readonly=True)
-    glpi_contact           = fields.Char('Utilisateur GLPI'  , readonly=True)
-    glpi_serial            = fields.Char('N°série'      , readonly=True)
-    glpi_os_license_number = fields.Char('Licence OS'   , readonly=True)
-    glpi_os_licenseid      = fields.Char('Licence OS id', readonly=True)
-    glpi_date_mod          = fields.Datetime('Date GLPI', readonly=True)
-    glpi_operatingsystems  = fields.Char('Système'      , readonly=True)
-
-    glpi_bios_date         = fields.Date('Date du bios', readonly=True)
-    glpi_installationdate  = fields.Date("Date d'installation", readonly=True)
-    glpi_remote_addr       = fields.Text('Adresse IP'      , readonly=True)
-    glpi_adresse_ip_mac    = fields.Text('Adresse IP+MAC'  , readonly=True)
-    glpi_winowner          = fields.Char('Administrateur'  , readonly=True)
+    glpi_id_ordinateur           = fields.Integer('ID ordinateur GLPI', readonly=True)
+    glpi_nom_ordinateur          = fields.Char('Nom ordinateur GLPI', readonly=True)
+    glpi_numero_serie            = fields.Char('N° série', readonly=True)
+    glpi_fabricant               = fields.Char('Fabricant', readonly=True)
+    glpi_systeme_exploitation    = fields.Char('Système exploitation', readonly=True)
+    glpi_version_os              = fields.Char('Version OS', readonly=True)
+    glpi_version_bios            = fields.Char('Version BIOS', readonly=True)
+    glpi_date_bios               = fields.Date('Date BIOS', readonly=True)
+    glpi_adresses_mac            = fields.Char('Adresses MAC', readonly=True)
+    glpi_usager                  = fields.Char('Usager', readonly=True)
+    glpi_utilisateur_affecte     = fields.Char('Utilisateur affecté', readonly=True)
+    glpi_adresse_publique        = fields.Char('Adresse publique', readonly=True)
+    glpi_version_agent           = fields.Char('Version agent', readonly=True)
+    glpi_dernier_contact         = fields.Datetime('Dernier contact', readonly=True)
 
     net_rpc_users          = fields.Char('net rpc users'   , readonly=True)
     net_rpc_admins         = fields.Char('net rpc admins'  , readonly=True)
@@ -96,7 +97,7 @@ class is_ordinateur(models.Model):
         #db = MySQLdb.connect(host=glpi_host, user=glpi_user, passwd=glpi_passwd, db=glpi_db)
 
 
-        cur = db.cursor()
+        cur = db.cursor(MySQLdb.cursors.DictCursor)
         nb=len(self)
         ct=0
         for obj in self:
@@ -106,43 +107,59 @@ class is_ordinateur(models.Model):
             _logger.info(str(ct)+u'/'+str(nb)+u' : Mise à jour GLPI '+name)
 
             SQL="""
-                select
-                    c.name,
-                    c.contact,
-                    c.serial,
-                    '' os_license_number,
-                    '' os_licenseid,
-                    agents.last_contact,
-                    concat(os.name,' ',v.name),
-                    sp.name,
-                    '' bios_date,
-                    f.operatingsystem_installationdate,
-                    f.remote_addr,
-                    f.winowner
-                from glpi_computers c inner join glpi_items_operatingsystems    i on c.id=i.items_id
-                                      inner join glpi_operatingsystems         os on i.operatingsystems_id=os.id
-                                      inner join glpi_operatingsystemversions   v on i.operatingsystemversions_id=v.id
-                                      left outer join glpi_operatingsystemservicepacks sp on i.operatingsystemservicepacks_id=sp.id
-                                      left outer join glpi_plugin_fusioninventory_inventorycomputercomputers f on f.computers_id=c.id
-                                      left outer join glpi_plugin_fusioninventory_agents agents on c.id=agents.computers_id
-                WHERE c.name=%s
+                SELECT 
+                    c.id AS id_ordinateur,
+                    c.name AS nom_ordinateur,
+                    c.serial AS numero_serie,
+                    m.name AS fabricant,
+                    os.name AS systeme_exploitation,
+                    osv.name AS version_os,
+                    fw.designation AS version_bios,
+                    fw.date AS date_bios,
+                    GROUP_CONCAT(DISTINCT np.mac SEPARATOR ', ') AS adresses_mac,
+                    c.contact AS usager,
+                    CONCAT(u.firstname, ' ', u.realname) AS utilisateur_affecte,
+                    a.remote_addr AS adresse_publique,
+                    a.version AS version_agent,
+                    a.last_contact AS dernier_contact
+                FROM glpi_computers AS c
+                LEFT JOIN glpi_agents AS a ON (a.items_id = c.id AND a.itemtype = 'Computer')
+                LEFT JOIN glpi_manufacturers AS m ON c.manufacturers_id = m.id
+                LEFT JOIN glpi_items_operatingsystems AS ios ON (ios.items_id = c.id AND ios.itemtype = 'Computer')
+                LEFT JOIN glpi_operatingsystems AS os ON ios.operatingsystems_id = os.id
+                LEFT JOIN glpi_operatingsystemversions AS osv ON ios.operatingsystemversions_id = osv.id
+                LEFT JOIN glpi_items_devicefirmwares AS ifw ON (ifw.items_id = c.id AND ifw.itemtype = 'Computer' AND ifw.is_deleted = 0)
+                LEFT JOIN glpi_devicefirmwares AS fw ON ifw.devicefirmwares_id = fw.id
+                LEFT JOIN glpi_networkports AS np ON (np.items_id = c.id AND np.itemtype = 'Computer' AND np.is_deleted = 0)
+                LEFT JOIN glpi_users AS u ON c.users_id = u.id
+                WHERE c.is_deleted = 0
+                    AND c.is_template = 0
+                    AND c.name = %s
+                GROUP BY c.id, c.name, c.serial, c.otherserial, c.contact, m.name, os.name, osv.name, fw.designation, fw.date, u.firstname, u.realname, a.remote_addr, a.name, a.version, a.last_contact
+                ORDER BY c.name;
             """
 
             cur.execute(SQL, [name])
             for row in cur.fetchall():
-                obj.glpi_name              = row[0]
-                obj.glpi_contact           = row[1]
-                obj.glpi_serial            = row[2]
-                obj.glpi_os_license_number = row[3]
-                obj.glpi_os_licenseid      = row[4]
-                obj.glpi_date_mod          = row[5]
-                obj.glpi_operatingsystems  = (row[6] or '')+' '+(row[7] or '')
-                if row[8]:
-                    obj.glpi_bios_date     = row[8].strftime('%Y-%m-%d')  
-                if row[9]:
-                    obj.glpi_installationdate = row[9].strftime('%Y-%m-%d')  
-                obj.glpi_remote_addr       = row[10]
-                obj.glpi_winowner          = row[11]
+                obj.glpi_id_ordinateur        = row['id_ordinateur']
+                obj.glpi_nom_ordinateur       = row['nom_ordinateur']
+                obj.glpi_numero_serie         = row['numero_serie']
+                obj.glpi_fabricant            = row['fabricant']
+                obj.glpi_systeme_exploitation = row['systeme_exploitation']
+                obj.glpi_version_os           = row['version_os']
+                obj.glpi_version_bios         = row['version_bios']
+                if row['date_bios']:
+                    obj.glpi_date_bios        = row['date_bios'].strftime('%Y-%m-%d')
+                obj.glpi_adresses_mac         = row['adresses_mac']
+                obj.glpi_usager               = row['usager']
+                obj.glpi_utilisateur_affecte  = row['utilisateur_affecte']
+                obj.glpi_adresse_publique     = row['adresse_publique']
+                obj.glpi_version_agent        = row['version_agent']
+                obj.glpi_dernier_contact      = row['dernier_contact']
+
+
+
+
 
     def actualiser_glpi_scheduler_action(self, cr, uid, use_new_cursor=False, company_id = False, context=None):
         self.actualiser_glpi_scheduler(cr, uid, context)
